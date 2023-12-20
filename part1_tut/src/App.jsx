7e15d9f2218e557b8404937c8a4714fb68e49087
schrodingerszanycat <152669825+thebardofavon@users.noreply.github.com>
2023-12-20 09:41:20 +0530

    Make changes

--- part1_tut/src/App.jsx
+++ part1_tut/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from "react";
-
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
+import { useState } from 'react'
 const App = () => {
-    const [ counter, setCounter] = useState(0)
+   const [left, setLeft] = useState(0)
+   const [right, setRight] = useState(0)
+   const [allClicks, setAll] = useState([])
 
-    const increaseByOne = () => setCounter(counter + 1)
-    const setToZero = () => setCounter(0)
-    const decreaseByOne = () => setCounter(counter - 1)
+   const handleLeftClick = () => {
+       setLeft(left + 1)
+       setAll(allClicks.concat('L'))
+   }
 
-    return(
-        <div>
-            <Display counter={counter} />
-            <Button
-                onClick={increaseByOne}
-                text='Increment'
-            />
-            <Button
-                onClick={setToZero}
-                text='Reset'
-            />
-            <Button
-                onClick={decreaseByOne}
-                text='Decrement'
-            />
-        </div>
-    )
+   const handleRightClick = () => {
+       setRight(right + 1)
+       setAll(allClicks.concat('R'))
+   }
+
+   return (
+       <div>
+           {left}
+           <button onClick={handleLeftClick}>L</button>
+           <button onClick={handleRightClick}>R</button>
+           {right}
+           <p>{allClicks.join(' ')}</p>
+       </div>
+   )
 }
-
-
 export default App
